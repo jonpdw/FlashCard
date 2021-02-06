d770e6c3c8e757b14587fdbe9411e89e8058e5ac
Jonathan <jonpdw@gmail.com>
2021-02-07 03:43:04 +1300

    removing some stuff

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
-import Toast from "/dependency/Toast-ES6.js"
 
 let question = document.querySelector("#question")
-let button = document.querySelector("button")
 let answer = document.querySelector("#answer")
+let button = document.querySelector("button")
 let body = document.querySelector("body")
 
 question.currentTime = 25
+question.playbackRate = 1.3
+answer.playbackRate = 1.3
 
-question.onended = function() {
-    // answer.play()
+question.onended = function () {
     let next = document.createElement("h2")
     next.textContent = "Presss space for next"
-    body.insertBefore(next,question)
+    body.insertBefore(next, question)
 }
 
 window.addEventListener('keydown', (event) => {
     if (event.key === " ") {
-        alert
+        // Toastify({
+        //     text: "Space pressed",
+        //     duration: 3000,
+        //     close: true,
+        //     gravity: "bottom", // `top` or `bottom`
+        //     position: "left", // `left`, `center` or `right`
+        //     backgroundColor: "linear-gradient(to right, #1A73E8, #1A73E8)",
+        //     stopOnFocus: true, // Prevents dismissing of toast on hover
+        // }).showToast();
+        question.pause()
+        answer.play()
+
     }
 })
 
-button.addEventListener('click', () => {
-    new Toast({message: 'Welcome to Toast.js!'});
-})
 
